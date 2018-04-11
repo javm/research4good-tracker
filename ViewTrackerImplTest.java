ac69b7bed0d9e57cb213cccc1ae915c51b499414
javm <quetzal1910@gmail.com>
2018-04-11 14:15:08 -0500

    Adding more tests

--- ViewTrackerImplTest.java
+++ ViewTrackerImplTest.java
@@ -3,14 +3,19 @@ public class ViewTrackerImplTest {
   public static void main(String[] args) {
     ViewTrackerImpl vt = new ViewTrackerImpl();
     Collection<String> admins = vt.getAdmins("abc");
-    System.out.println(admins);
+    System.out.println("No admins visiting page abc: " + admins);
+    System.out.println("admin1 visiting page1");
     vt.trackView("admin1", "page1");
     Collection<String> realAdmins = vt.getAdmins("page1");
     System.out.println("page1: " + realAdmins);
     Collection<String> realPages = vt.getPages("admin1");
-    System.out.println("admi1: " + realPages);
+    System.out.println("admin1: " + realPages);
+    System.out.println("admin1 visiting page2");
     vt.trackView("admin1", "page2");
+    System.out.println("admin2 visiting page2");
     vt.trackView("admin2", "page2");
+    Collection<String> page2AdminCol = vt.getAdmins("page2");
+    System.out.println("page2: " + page2AdminCol);
     (new Thread( () -> {
       try{
         Thread.sleep(25000);
@@ -19,9 +24,11 @@ public class ViewTrackerImplTest {
       }
 
       Collection<String> againAdmins = vt.getAdmins("page1");
-      System.out.println(againAdmins);
+      System.out.println("page1: " + againAdmins);
       Collection<String> againPages = vt.getPages("admin1");
-      System.out.println(againPages);
+      System.out.println("admin1: " + againPages);
+      Collection<String> page2Admins = vt.getAdmins("page2");
+      System.out.println("page2: " + page2Admins);
     } )).start();
   }
 }
